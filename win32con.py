aef0f8aa7e5b017456bb0b6effb3efcadf42934d
Charles Grunwald <ch@rles.rocks>
2016-03-06 00:19:13 -0800

    Removed win32clipboard dependency from InputState.py

--- win32con.py
+++ win32con.py
@@ -3,9 +3,23 @@ win32con.py
 Minimal fill-in for pywin32's win32con module. Defines only the constants used by PyCmd.
 """
 
+## Win32 Error Codes
+ERROR_SUCCESS = 0
+
 ## Console Key Stuff
 RIGHT_ALT_PRESSED = 0x0001  # the right alt key is pressed.
 LEFT_ALT_PRESSED = 0x0002  # the left alt key is pressed.
 RIGHT_CTRL_PRESSED = 0x0004  # the right ctrl key is pressed.
 LEFT_CTRL_PRESSED = 0x0008  # the left ctrl key is pressed.
 SHIFT_PRESSED = 0x0010  # the shift key is pressed.
+
+## Clipboard Formats
+CF_TEXT = 1
+CF_OEMTEXT = 7
+CF_UNICODETEXT = 13
+
+## HGLOBAL Management
+GHND = 0x0042
+GPTR = 0x0040
+GMEM_MOVEABLE = 0x0002
+GMEM_ZEROINIT = 0x0040
